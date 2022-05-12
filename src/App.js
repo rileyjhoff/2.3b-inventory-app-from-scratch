3d88d0117700f8942ee2e7e6ba178dce8c77424c
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-12 09:55:11 -0700

    add supabase sign in, sign out, logout, and getUser functions. App checks for user using getUser and conditionally renders header with NavLink components, and redirects the user back to AuthPage if they are not signed in

--- src/App.js
+++ src/App.js
@@ -6,22 +6,43 @@ import AuthPage from './components/AuthPage';
 import ListPage from './components/ListPage';
 import CreatePage from './components/CreatePage';
 import UpdatePage from './components/UpdatePage';
+import { useEffect, useState } from 'react';
+import { getUser, logout } from './services/fetch-utils.js';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const user = getUser();
+    setUser(user);
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <header>
-          <NavLink to="/list">Home</NavLink>
-          <NavLink to="/create">Create</NavLink>
-          <Button>Logout</Button>
+          {user && (
+            <>
+              <NavLink to="/list">Home</NavLink>
+              <NavLink to="/create">Create</NavLink>
+              <Button>Logout</Button>
+            </>
+          )}
         </header>
         <main>
           <Switch>
-            <Route exact path="/"></Route>
-            <Route exact path="/list"></Route>
-            <Route exact path="/item/:id"></Route>
-            <Route exact path="/create"></Route>
+            <Route exact path="/">
+              {user ? <Redirect to="/list" /> : <AuthPage />}
+            </Route>
+            <Route exact path="/list">
+              {user ? <ListPage /> : <Redirect to="/" />}
+            </Route>
+            <Route exact path="/item/:id">
+              {user ? <UpdatePage /> : <Redirect to="/" />}
+            </Route>
+            <Route exact path="/create">
+              {user ? <CreatePage /> : <Redirect to="/" />}
+            </Route>
           </Switch>
         </main>
       </div>
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,23 @@
 import { client, checkError } from './client.js';
+
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+
+  return (window.location.href = '../');
+}
+
+export function getUser() {
+  return client.auth.session();
+}
