408b4500b1d74424b9f6715ad134ea84b23687ea
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-12 16:54:15 -0700

    wrote supabase CRUD functions

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,4 +1,4 @@
-import { client } from './client.js';
+import { client, checkError } from './client.js';
 
 export async function signUp(email, password) {
   const response = await client.auth.signUp({ email, password });
@@ -20,4 +20,34 @@ export async function logout() {
 
 export function getUser() {
   return client.auth.session();
+}
+
+export async function getAllScores() {
+  const response = await client.from('scores').select('*').order('date', { ascending: false });
+
+  return checkError(response);
+}
+
+export async function getScoreById(id) {
+  const response = await client.from('scores').select('*').match({ id });
+
+  return checkError(response);
+}
+
+export async function updateScore(game) {
+  const response = await client.from('scores').update([game]).match({ id: game.id });
+
+  return checkError(response);
+}
+
+export async function createScore(game) {
+  const response = await client.from('scores').insert([game]);
+
+  return checkError(response);
+}
+
+export async function deleteScore(id) {
+  const response = await client.from('scores').delete().match({ id });
+
+  return checkError(response);
 }
