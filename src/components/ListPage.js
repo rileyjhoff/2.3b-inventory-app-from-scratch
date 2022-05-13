012800e6131d06553770f0cd3815585edf330755
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-12 17:02:00 -0700

    ListPage calls getAllScores on page load, sets the score state with an array of scores. scores is mapped through and a ListItm is rendered for each item in the scores array

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllScores } from '../services/fetch-utils';
+import ListItem from './ListItem';
 
 export default function ListPage() {
-  return <div>ListPage</div>;
+  const [scores, setScores] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const scores = await getAllScores();
+      setScores(scores);
+    }
+    load();
+  });
+
+  return (
+    <div className="list-page">
+      {scores.map((score, i) => (
+        <ListItem key={i} score={score} />
+      ))}
+    </div>
+  );
 }
